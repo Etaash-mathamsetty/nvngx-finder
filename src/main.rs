d7aa70cef59f06adf129241a0cf23daae48fc5b1
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2023-04-02 12:44:30 -0400

    Initial code

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::ffi::{CString, CStr};
+use libc::{dlopen, dlinfo, dlerror, RTLD_NOW, RTLD_DI_LINKMAP, c_void};
+use std::ptr::null_mut;
+use std::mem::transmute;
+use std::path::Path;
+
+#[allow(dead_code)]
+struct LinkMap
+{
+    j1: *mut c_void,
+    l_name: *mut i8,
+    j2: *mut c_void,
+    next: *mut LinkMap,
+    prev: *mut LinkMap,
+}
+
+fn main() {
+    let nvngx_lib = CString::new("libGLX_nvidia.so.0").expect("failed to create CString");
+    let nvngx: *mut c_void = unsafe { dlopen(nvngx_lib.as_ptr(), RTLD_NOW) };
+
+    if nvngx.is_null()
+    {
+        unsafe { panic!("dlopen failed {}", CStr::from_ptr(dlerror()).to_str().expect("failed to convert to str")) };
+    }
+
+    let mut info: *mut c_void = null_mut();
+    let ret: i32 = unsafe { dlinfo(nvngx, RTLD_DI_LINKMAP, transmute(&mut info)) };
+
+    if ret != 0
+    {
+        panic!("dlinfo failed with ret {:?}", ret);
+    }
+
+    let link_map: *mut LinkMap =  info as *mut LinkMap;
+
+    let mut path = unsafe { Path::new(CStr::from_ptr((*link_map).l_name).to_str().expect("failed to convert to str")) };
+    path = path.parent().expect("failed to get parent directory");
+
+    println!("{}", path.to_str().expect("failed to unwrap path"));
+}
